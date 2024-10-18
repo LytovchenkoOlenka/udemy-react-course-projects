fc175db092794ea276dcfbb8c6ffae2ddcb70ab3
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-10-18 13:13:37 +0200

    Updated_styled&added_conditional_rendering
    Section3.55-57.

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,12 +1,28 @@
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton.jsx";
-import { CORE_CONCEPTS } from "./data.js";
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
+import { useState } from "react";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
   function handleClick(selectedButton) {
     //selectedButton=>'components','lsx','props','state'
+    setSelectedTopic(selectedButton);
   }
+
+  // Cпосіб умовного рендерингу через if
+  //let tabContent =  <p>Please select a topic.</p>;
+  // if(selectedTopic){
+  //   tabContent = <div id="tab-content">
+  //   <h3>{EXAMPLES[selectedTopic].title}</h3>
+  //   <p>{EXAMPLES[selectedTopic].description}</p>
+  //   <pre>
+  //     <code>{EXAMPLES[selectedTopic].code}</code>
+  //   </pre>
+  // </div>
+  // }
 
   return (
     <div>
@@ -29,13 +45,58 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleClick("components")}>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onSelect={() => handleClick("components")}
+            >
               Components
             </TabButton>
-            <TabButton onSelect={() => handleClick("jsx")}>JSX</TabButton>
-            <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
-            <TabButton onSelect={() => handleClick("state")}>State</TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onSelect={() => handleClick("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onSelect={() => handleClick("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onSelect={() => handleClick("state")}
+            >
+              State
+            </TabButton>
           </menu>
+          {/* Умовний рендеринг через тернарний оператор */}
+          {!selectedTopic ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
+
+          {/* Альтернативний спосіб умовного рендерингу через оператор && */}
+          {/* {!selectedTopic && <p>Please select a topic.</p>}
+          {!selectedTopic && (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )} */}
+
+          {/*Cпосіб умовного рендерингу через if */}
+          {/* {tabContent} */}
         </section>
       </main>
     </div>
