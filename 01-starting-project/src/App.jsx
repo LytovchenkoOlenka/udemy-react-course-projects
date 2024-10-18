67d73af4f3d15342992408bfeb1c804af0867d8a
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-10-18 13:42:30 +0200

    refactored_the_code
    Added dynamic outputting. Section3.58

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -31,15 +31,20 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
+            {/* Рефакторінг. Перебираємо масив і динамічно почергово виводимо кожент елемент масиву  */}
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
+            {/* Виведення кожної концепції вручну  */}
+            {/* <CoreConcept {...CORE_CONCEPTS[0]} />
+                    <CoreConcept {...CORE_CONCEPTS[1]} />
+                    <CoreConcept {...CORE_CONCEPTS[2]} /> */}
             {/* Використання пропсів в менш оптимальному варіанті  */}
-            <CoreConcept
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              image={CORE_CONCEPTS[3].image}
-            />
+            {/* <CoreConcept
+                      title={CORE_CONCEPTS[3].title}
+                      description={CORE_CONCEPTS[3].description}
+                      image={CORE_CONCEPTS[3].image}
+                    /> */}
           </ul>
         </section>
         <section id="examples">
