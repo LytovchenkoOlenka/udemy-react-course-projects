da1cc668bf01e0ad8b61d71da3eae36bf71ee7b3
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-10-22 20:17:30 +0200

    Added_PlayerComponent.
    Section-4.73-79.
    Concept Repetition: Splitting Components & Building Reusable Components.
    User Input & Two-Way-Binding

---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+  return (
+    <li>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleEditChange}
+          />
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
