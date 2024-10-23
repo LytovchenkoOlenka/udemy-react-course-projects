1b612b9dfb5cbd44e4ba5542c2da1d622d04d43d
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-10-23 18:57:00 +0200

    created_GameBoard.jsx
    Section4.
    80. Rendering Multi-Dimensional Lists
    81.  Best Practice: Updating Object State Immutably

---
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const inintialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(inintialGameBoard);
+
+  function handleSelectSqure(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSqure(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
