a3e89b1117a6f8adae86595b4611b7ef803fded8
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-10-25 21:53:03 +0200

    Updated_UserInput

--- 05-investment-calculator-project/src/components/UserInput.jsx
+++ 05-investment-calculator-project/src/components/UserInput.jsx
@@ -2,10 +2,27 @@ export default function UserInput() {
   return (
     <section id="user-input">
       <div className="input-group">
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
+        <p>
+          <label>Initial Investment </label>
+          <input type="number" required />
+        </p>
+
+        <p>
+          <label>Anual Investment </label>
+          <input type="number" required />
+        </p>
+      </div>
+
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input type="number" required />
+        </p>
+
+        <p>
+          <label>Duration </label>
+          <input type="number" required />
+        </p>
       </div>
     </section>
   );
