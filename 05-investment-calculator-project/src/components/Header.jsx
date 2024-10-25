6f590aa4ad5b21f545ea86b712301a64784c3448
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-10-25 21:37:51 +0200

    Updated_Header

--- 05-investment-calculator-project/src/components/Header.jsx
+++ 05-investment-calculator-project/src/components/Header.jsx
@@ -1,8 +1,10 @@
+import logo from "../assets/investment-calculator-logo.png";
+
 export default function Header() {
   return (
     <header id="header">
       <img
-        src="investment-calculator-logo.png"
+        src={logo}
         alt="A green bag with a gold dollar sign, surrounded by coins and bars, symbolizing wealth or finance."
       />
       <h1>React Investment Calculator</h1>
