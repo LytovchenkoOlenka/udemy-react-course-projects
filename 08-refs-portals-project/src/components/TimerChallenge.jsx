6e2dc0d36fd9cffd99f7fa6ca1b7fe562152b165
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-11-02 21:06:05 +0100

    fixed_modal

--- 08-refs-portals-project/src/components/TimerChallenge.jsx
+++ 08-refs-portals-project/src/components/TimerChallenge.jsx
@@ -10,8 +10,12 @@ export default function TimerChallenge({ title, targetTime }) {
   const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
   if (timeRemaining <= 0) {
     clearInterval(timer.current);
-    setTimeRemaining(targetTime * 1000);
+
     dialog.current.open();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
   }
 
   function handleStart() {
@@ -27,7 +31,12 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={remainingTime}
+        onReset={handleReset}
+      />
 
       <section className="challenge">
         <h2>{title}</h2>
