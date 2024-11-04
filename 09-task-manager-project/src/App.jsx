2a3b12d2f1b7bf33000b2c783a04861b0c4111dd
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-11-04 20:24:12 +0100

    added_Managing State to Switch Between Components

--- 09-task-manager-project/src/App.jsx
+++ 09-task-manager-project/src/App.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import SideBar from "./components/SideBar";
 import AddProject from "./components/AddProject";
-import DefaultMain from "./components/DefaultMain";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectCard from "./components/ProjectCard";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    currentAction: "",
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
-      <SideBar />
-      <AddProject />
-      {/* <DefaultMain /> */}
+      <SideBar onAddProject={handleAddProject} />
+      {projectsState.selectedProjectId === null && <AddProject />}
+      {projectsState.selectedProjectId === undefined && (
+        <NoProjectSelected onAddProject={handleAddProject} />
+      )}
+
+      {/* <ProjectCard /> */}
     </main>
   );
 }
