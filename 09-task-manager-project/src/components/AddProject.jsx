a1be9973050272d2143d4e4fed14ca1cc4a4245f
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-11-06 17:56:43 +0100

    created_reusable_modal

--- 09-task-manager-project/src/components/AddProject.jsx
+++ 09-task-manager-project/src/components/AddProject.jsx
@@ -1,7 +1,10 @@
 import Input from "./Input";
 import { useRef, useState } from "react";
+import Modal from "./Modal";
 
 export default function AddProject({ onAdd }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -11,7 +14,15 @@ export default function AddProject({ onAdd }) {
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
 
-    //validation...
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
+
     onAdd({
       title: enteredTitle,
       description: enteredDescription,
@@ -20,28 +31,39 @@ export default function AddProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            type="submit"
-            onClick={handleSave}
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <form>
-        <Input ref={title} label="Title" type="text'" />
-        <Input ref={description} label="Description" textarea />
-        <Input ref={dueDate} label="Due date" type="date" />
-      </form>
-    </div>
+    <>
+      <Modal ref={modal} buttonCaption="Close">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Oops ... looks like you forgot to enter a value.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              type="submit"
+              onClick={handleSave}
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <form>
+          <Input ref={title} label="Title" type="text'" />
+          <Input ref={description} label="Description" textarea />
+          <Input ref={dueDate} label="Due date" type="date" />
+        </form>
+      </div>
+    </>
   );
 }
