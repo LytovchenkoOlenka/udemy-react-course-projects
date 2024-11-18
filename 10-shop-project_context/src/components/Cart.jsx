53d90d816106c07755d1838dd5a0ca858d16884d
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2024-11-18 16:19:41 +0100

    usedContext
    167. Creating & Providing The Context
    168. Consuming the Context
    169. Linking the Context to State

--- 10-shop-project_context/src/components/Cart.jsx
+++ 10-shop-project_context/src/components/Cart.jsx
@@ -1,5 +1,10 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
-  const totalPrice = items.reduce(
+import {useContext} from "react";
+import {CartContext} from "../store/shopping-cart-context.jsx";
+
+export default function Cart({ onUpdateItemQuantity }) {
+  const {items}=useContext(CartContext)
+
+    const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
   );
