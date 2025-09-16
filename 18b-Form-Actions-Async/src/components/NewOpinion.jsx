b16c0d4f235f6cd54f6059cb01cb73aa66866fb6
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-09-16 16:55:15 +0200

    add handling forms via Form Actions

--- 18b-Form-Actions-Async/src/components/NewOpinion.jsx
+++ 18b-Form-Actions-Async/src/components/NewOpinion.jsx
@@ -1,27 +1,96 @@
+import { useActionState, use } from "react";
+import { OpinionsContext } from "../store/opinions-context";
+import Submit from "./Submit";
+
 export function NewOpinion() {
+  const { addOpinion } = use(OpinionsContext);
+
+  async function sharingOpinionAction(prevFormState, formData) {
+    const userName = formData.get("userName");
+    const title = formData.get("title");
+    const body = formData.get("body");
+
+    let errors = [];
+
+    if (userName.trim() == "" || userName.length <= 3) {
+      errors.push("Error name");
+      console.log("Error name");
+    }
+
+    if (title.trim() == "") {
+      errors.push("Error title");
+      console.log("Error title");
+    }
+
+    if (body.trim() == "" || body.length <= 6) {
+      errors.push("Error text");
+      console.log("Error text");
+    }
+
+    if (errors.length > 0) {
+      return {
+        errors,
+        enteredValues: {
+          userName,
+          title,
+          text,
+        },
+      };
+    }
+
+    await addOpinion({ userName, title, body });
+
+    return { errors: null };
+  }
+
+  const [formState, formAction] = useActionState(sharingOpinionAction, {
+    errors: null,
+  });
+
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
-      <form>
+      <form action={formAction}>
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
-            <input type="text" id="userName" name="userName" />
+            <input
+              type=""
+              id="userName"
+              name="userName"
+              defaultValue={formState.enteredValues?.userName}
+            />
           </p>
 
           <p className="control">
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" />
+            <input
+              type=""
+              id="title"
+              name="title"
+              defaultValue={formState.enteredValues?.title}
+            />
           </p>
         </div>
         <p className="control">
           <label htmlFor="body">Your Opinion</label>
-          <textarea id="body" name="body" rows={5}></textarea>
+          <textarea
+            id="body"
+            name="body"
+            rows={5}
+            defaultValue={formState.enteredValues?.body}
+          ></textarea>
         </p>
 
-        <p className="actions">
-          <button type="submit">Submit</button>
-        </p>
+        {formState.errors && (
+          <ul className="errors">
+            {formState.errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
+
+        <Submit />
       </form>
     </div>
   );
