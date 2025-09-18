9c8c3af8ff0f0655bc5f7ab4a81e95d68d25dad2
LytovchenkoOlenka <131744554+LytovchenkoOlenka@users.noreply.github.com>
2025-09-18 17:28:08 +0200

    create_Header

---
+++ Food-Order-App/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
+
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="logo" />
+        <h1>ReactFood</h1>
+      </div>
+      <nav>
+        <Button textOnly>Cart(3)</Button>
+      </nav>
+    </header>
+  );
+}
